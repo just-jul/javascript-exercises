16460573182c91b311dc7912a2352b76886d53a2
just-jul <businessbyjul@gmail.com>
2025-09-07 11:52:15 +0100

    Complete exercise 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degrees) {
+  let result = (degrees - 32) * (5/9);
+	return parseFloat(result.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degrees) {
+  let result = degrees * (9/5) + 32;
+	return parseFloat(result.toFixed(1));
 };
 
 // Do not edit below this line
