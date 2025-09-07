f85b906a4144f4550fd9b230e8f3526bebc6e2fb
just-jul <businessbyjul@gmail.com>
2025-09-07 11:18:16 +0100

    Complete exercises 06, 07, 08

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,7 @@
-const reverseString = function(words) {
-    return words.split('').reverse().join('');
-};
+const reverseString = function(str){
+    return str.split('').reverse().join('');
+}
+
 
 // Do not edit below this line
 module.exports = reverseString;
--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-};
+const removeFromArray = (arr, ...args) => {
+    return arr.filter(element => !args.includes(element));
+}
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
