19413f624422d66abcbf6f0735347360d1bd5b93
just-jul <businessbyjul@gmail.com>
2025-09-25 15:50:07 +0100

    Complete exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
 
+    if (n < 0) return "OOPS";
+    const fib = [0, 1];
+    for (let i = 2; i <= n; i++){
+        fib.push(fib[i-1] + fib[i-2]);
+    }
+    return fib[n];
 };
 
 // Do not edit below this line
