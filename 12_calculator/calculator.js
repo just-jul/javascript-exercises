04b935d931441858043ed321fac2991c02e46ec1
just-jul <businessbyjul@gmail.com>
2025-09-25 13:29:02 +0100

    Complete exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+    return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+    return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce( (sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  return arr.reduce((sum, current) => sum * current);
+}; 
 
+const power = function(num, power) {
+	return Math.pow(num, power);
 };
 
-const power = function() {
-	
-};
+const factorial = function(num) {
 
-const factorial = function() {
-	
+  if (num < 0) return undefined;
+
+	let result = 1;
+
+  for (let i=2; i <= num; i++){
+    result *= i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
