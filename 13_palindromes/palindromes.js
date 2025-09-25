09405bee80ea0ed10206b13236fcb90a402d3c4c
just-jul <businessbyjul@gmail.com>
2025-09-25 14:46:34 +0100

    Complete exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let replaced = string.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    
+    let left = 0;
+    let right = replaced.length - 1;
 
+    while (left < right){
+        if (replaced[left] !== replaced[right]) return false;
+        left++;
+        right--;
+    }
+
+    return true;
 };
 
 // Do not edit below this line
